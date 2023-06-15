ada9c2b39da93527e8b82c920b6235e3d59d9099
artemio_rodriguez <artemio.rodriguez@inoventrix.com>
2023-06-15 01:38:20 -0600

    ADD: Process to calculate SS and match a driver
    - Add functions to count vowels and consonants
    - Add Function to get the factors of a number
    - Add Function to know if has more than one Factors between driver and destination
    - The task is not complete because need to use only one time each driver

--- app/Console/Commands/ShipmentAssignment.php
+++ app/Console/Commands/ShipmentAssignment.php
@@ -33,7 +33,7 @@ class ShipmentAssignment extends Command
     /**
      * Execute the console command.
      *
-     * @return int
+     * @return void
      */
     public function handle()
     {
@@ -45,9 +45,80 @@ class ShipmentAssignment extends Command
         $drivers = file($driversPath, FILE_IGNORE_NEW_LINES);
         $destinations = file($destinationsPath, FILE_IGNORE_NEW_LINES);
 
-        // Print Array From .txt files
-        print_r($drivers);
-        print_r($destinations);
-        return 0;
+        // Declare vars to print the result
+        $matchesDD = [];
+        $totalSS = 0;
+
+        // Iterate Destinations to calculate SS (suitability score) and find the best driver match for each destination
+        foreach ($destinations as $destination) {
+            $destinationLength = strlen($destination);
+            $bestScore = 0;
+            $bestMatch = '';
+
+            // find the best Driver for Destination
+            foreach ($drivers as $driver) {
+                $driverLength = strlen($driver);
+
+                // Verify is destination is even or odd with a module 2 to get the driver SS
+                $baseScore = ($destinationLength % 2 === 0)
+                    ? $this->countVowels($driver) * 1.5
+                    : $this->countConsonants($driver);
+
+                $score = $baseScore;
+
+                // Check if Destination and Driver has common Factors
+                if ($this->hasCommonFactors($destinationLength, $driverLength)) {
+                    $score *= 1.5;
+                }
+
+                // Compare score with bestScore to define the best driver for current destination
+                if ($score > $bestScore) {
+                    $bestScore = $score;
+                    $bestMatch = $driver;
+                }
+            }
+
+            $totalSS += $bestScore;
+            $matchesDD[$destination] = $bestMatch;
+        }
+
+        // Display the total SS and matching between destinations and drivers
+        printf($totalSS);
+        print("\n");
+        print_r($matchesDD);
+    }
+
+    private function countVowels($string)
+    {
+        $vowels = ['a', 'e', 'i', 'o', 'u'];
+        return count(array_intersect(str_split(strtolower($string)), $vowels));
+    }
+
+    private function countConsonants($string)
+    {
+        $vowels = ['a', 'e', 'i', 'o', 'u'];
+        return count(array_diff(str_split(strtolower($string)), $vowels));
+    }
+
+    // Review if Destination and Driver has more than 1 factors in common
+    // to make sure the point 3 of algorithm
+    private function hasCommonFactors($destination, $driver)
+    {
+        $factorsDestination = $this->getFactors($destination);
+        $factorsDriver = $this->getFactors($driver);
+
+        return count(array_intersect($factorsDestination, $factorsDriver)) > 1;
+    }
+
+    // Get the Factor of a strlength
+    private function getFactors($number)
+    {
+        $factors = [];
+        for ($i = 2; $i <= $number / 2; $i++) {
+            if ($number % $i === 0) {
+                $factors[] = $i;
+            }
+        }
+        return $factors;
     }
 }
